a2056043a68ed7223b0e4ed1c0c12fc5881779cf
JonathanNoH <jonathan897h@gmail.com>
2022-01-25 13:34:35 -0700

    do 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    const splitString = str.split('');
+    let newString = "";
+    for(i=1; i <= str.length; i++) {
+        newString += splitString[str.length-i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
