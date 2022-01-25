b42aaa1e82c5c61e8bda66ac3aa8765f7ff3d812
JonathanNoH <jonathan897h@gmail.com>
2022-01-25 13:59:17 -0700

    do 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const args = Array.from(arguments);
+    console.log(args);
+    const originalArray = args.shift();
+    console.log(originalArray);
+    function checkRemove(item){
+        for (let arg of args) {
+            if (item === arg) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const newArray = originalArray.filter(checkRemove);
+    return newArray;
 };
 
 // Do not edit below this line
