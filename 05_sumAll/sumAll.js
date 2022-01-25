46ecdd3218c1b699c1468bc384647c45ab1d3b01
JonathanNoH <jonathan897h@gmail.com>
2022-01-25 14:08:51 -0700

    do 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) ) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let smaller = (a < b) ? a:b;
+    let larger = (b > a) ? b:a;
+    for (; smaller <= larger; smaller++){
+        sum += smaller;
+    }
+    return sum;
 };
 
 // Do not edit below this line
