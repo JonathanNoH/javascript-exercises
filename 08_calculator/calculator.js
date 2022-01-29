82db0228d0f94be896baa9048949b51f627fc1e5
JonathanNoH <jonathan897h@gmail.com>
2022-01-29 15:31:19 -0700

    do 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	if (a == NaN || b == NaN) return;
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	if (a == NaN || b == NaN) return;
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((a,b) => a * b, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let total = 1
+  for (let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
