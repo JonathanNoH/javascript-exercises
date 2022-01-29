c18035585a51d255f96c42387d5ccc04269343f3
JonathanNoH <jonathan897h@gmail.com>
2022-01-29 16:16:33 -0700

    gave up

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    s.toLowerCase();
+    s.replace(" ", "");
+    const myArray = s.split("");
+    
+    const newStr = myArray.toString();
+    newStr.replace(",","");
+    return (newStr == s);
 };
 
 // Do not edit below this line
